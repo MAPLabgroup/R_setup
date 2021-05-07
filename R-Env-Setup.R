17fb51d7040ee942871b58b575cdf7b31d1314bf
Jon Starnes <jon-starnes@users.noreply.github.com>
2021-05-07 12:53:48 -0400

    Updated R setup
    Revised the installation and require setup for R packages, and added instructions for Rtools

--- R-Env-Setup.R
+++ R-Env-Setup.R
@@ -1,28 +1,74 @@
-#################### New R Install Set-up script ####################
+#################### R Package Set-up script ####################
 
-'''
-Set working directory. This should be the same for both Linux workstations. Change 'your_username' to yours specifically
-We are using the file server below - accessble from Linux workstations
-'''
-#setwd('/Users/your_username/Documents/your_folder') # From a Mac - Change 'your_username' to yours specifically
-# repo0 = "http://cran.us.r-project.org" # Original R repository if you want to save it as an object 
+# Installing the Rtools application externally is advised 
 
-setwd('/home/your_username/brown_lab')  # Linux
+# Require rstudioapi and install if not present 
+if (!require(rstudioapi)) {install.packages("rstudioapi"); require(rstudioapi)}
+if (!require(installr)) {install.packages("installr"); require(installr)}
+if (!require(pkgbuild)) {install.packages("pkgbuild"); require(pkgbuild)}
 
-'''
-Short list of more commonly used packages for data wrangling (doBy, dplyr) and plotting graphics (ggthemes etc.) and working with external 
-programs (rprime and R.matlab)
-''' 
-pkg.list = c('ez', 'ggthemes', 'RColorBrewer', 'rprime', 'R.matlab', 'doBy', 'dplyr', 'psych', 'skimr', 'stringi', 'tidyr') 
+
+# Set current dir to script location 
+currentWD = dirname(rstudioapi::getActiveDocumentContext()$path)
+
+# Set working directory. use the automated script location method or set manually 
+# If manually setting your WD, change below to match OS directory paths 
+#setwd('/Users/your_username/Documents/your_folder')  
+
+
+# if sourcing this script with Source() run setwd() in the parent script 
+# after sourcing this one
+setwd(currentWD) 
+
+
+# Checks for Rtools - Stop script run here if you need to install it
+# Checks and then opens the Rtools website 
+if(has_rtools() == TRUE) { 
+  browseURL("https://cran.r-project.org/bin/windows/Rtools/"); 
+  # installr for rtools is not working as expected. Bug reported here:
+  # https://github.com/talgalili/installr/issues/158
+  #installr::install.rtools(choose_version = T, GUI = T, 
+  #                         page_with_download_url = "https://cran.r-project.org/bin/windows/Rtools/") 
+  print("Download Rtools and install.");
+#    print("Start script on the line below where indicated") 
+  }
+
+
+# For Manual install of  Rtools 40 + use the link below 
+#browseURL("https://cran.r-project.org/bin/windows/Rtools/")
+
+
+################################################################################
+
+###################### START HERE AFTER INSTALLING RTools ######################
+
+
+# After install this line will write path to system  
+#writeLines('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', con = "~/.Renviron")
+
+
+# Short list of more commonly used packages 
+pkg.list = c('devtools', 'dplyr', 'ez', 'ggthemes', 'Hmisc', 
+             'openxlsx', 'pkgbuild', 'psych', 'RColorBrewer', 
+             'R.matlab', 'rprime', 'skimr', 'stringi', 'tidyr') 
+
+
 doInstall <- TRUE  # Change to FALSE if you don't want packages installed.
 toInstall <- pkg.list
+# Install packages from list above 
+if(doInstall){install.packages(toInstall, dependencies=TRUE)}
+lapply(toInstall, require, character.only = TRUE)
 
-# Install the packages we specified above in pkg.list 
-if(doInstall){install.packages(toInstall, dependencies=TRUE, repos='http://cran.rstudio.com/')}
-lapply(toInstall, library, character.only = TRUE) 
 
-# I install ggplot seperately because it takes 10x longer than anything else. Easier debugging
-install.packages('ggplot2', dependencies=TRUE, repos='http://cran.rstudio.com/')
+# I install ggplot separately because it takes 10x longer than anything else. Easier debugging
+if(!require(ggplot2)) {install.packages("ggplot2", dependencies=TRUE); 
+  require(rstudioapi)}
+
+
 #install.packages('Cairo','http://www.rforge.net/') # optional package for saving hi-res plots while using Windows. 
-#Mac and Linux automatically output and save hi-res graphics in R. Don't know why Windows doesn't. Uncomment above line
-#to use if you are running R and RStudio from windows. 
+# Mac and Linux automatically output and save hi-res graphics in R. 
+# Use if you are running R and RStudio from windows. 
+
+
+# Done 
+print("Setup complete. Script resuming.")
