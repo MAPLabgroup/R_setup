de9f8903b9796d26b3007e43256a3695231d9126
Jon Starnes <jon-starnes@users.noreply.github.com>
2021-07-16 10:31:35 -0400

    Update R-Env-Setup.R

--- R-Env-Setup.R
+++ R-Env-Setup.R
@@ -1,6 +1,9 @@
 #################### R Package Set-up script ####################
 
-# Installing the Rtools application externally is advised 
+# Install the Rtools application 
+# Download here 
+# https://cran.r-project.org/bin/windows/Rtools/
+
 
 # Require rstudioapi and install if not present 
 if (!require(rstudioapi)) {install.packages("rstudioapi"); require(rstudioapi)}
@@ -22,17 +25,18 @@ setwd(currentWD)
 
 
 # Checks for Rtools - Stop script run here if you need to install it
-# Checks and then opens the Rtools website 
-if(has_rtools() == TRUE) { 
-  browseURL("https://cran.r-project.org/bin/windows/Rtools/"); 
+# Checks and then opens the Rtools website  
+
+
+#if(has_rtools() == TRUE) { 
+#  browseURL("https://cran.r-project.org/bin/windows/Rtools/"); 
   # installr for rtools is not working as expected. Bug reported here:
   # https://github.com/talgalili/installr/issues/158
   #installr::install.rtools(choose_version = T, GUI = T, 
   #                         page_with_download_url = "https://cran.r-project.org/bin/windows/Rtools/") 
-  print("Download Rtools and install.");
+ # print("Download Rtools and install.");
 #    print("Start script on the line below where indicated") 
-  }
-
+#  }
 
 # For Manual install of  Rtools 40 + use the link below 
 #browseURL("https://cran.r-project.org/bin/windows/Rtools/")
@@ -48,15 +52,19 @@ if(has_rtools() == TRUE) {
 
 
 # Short list of more commonly used packages 
-pkg.list = c('devtools', 'dplyr', 'ez', 'ggthemes', 'Hmisc', 
-             'openxlsx', 'pkgbuild', 'psych', 'RColorBrewer', 
-             'R.matlab', 'rprime', 'skimr', 'stringi', 'tidyr') 
+pkg.list = c('caret', 'corrplot', 'devtools', 'dplyr', 'ez', 'ggthemes', 'Hmisc', 
+             'lubridate', 'openxlsx', 'pkgbuild', 'psych', 'RColorBrewer', 
+             'skimr', 'stringi', 'tidyr') 
 
 
-doInstall <- TRUE  # Change to FALSE if you don't want packages installed.
+#doInstall <- TRUE  # Change to FALSE if you don't want packages installed.
 toInstall <- pkg.list
+
 # Install packages from list above 
-if(doInstall){install.packages(toInstall, dependencies=TRUE)}
+#if(doInstall){install.packages(toInstall, dependencies=TRUE)}
+# 
+install.packages(setdiff(toInstall, rownames(installed.packages())))
+
 lapply(toInstall, require, character.only = TRUE)
 
 
@@ -65,7 +73,8 @@ if(!require(ggplot2)) {install.packages("ggplot2", dependencies=TRUE);
   require(rstudioapi)}
 
 
-#install.packages('Cairo','http://www.rforge.net/') # optional package for saving hi-res plots while using Windows. 
+# Optional -- for windows only 
+# install.packages('Cairo','http://www.rforge.net/') # optional package for saving hi-res plots while using Windows. 
 # Mac and Linux automatically output and save hi-res graphics in R. 
 # Use if you are running R and RStudio from windows. 
 
